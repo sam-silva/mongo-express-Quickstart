aee5d6554b96428645fc6f094ac75261814e80f4
Samuel Silva <sdjsz7187@gmail.com>
2020-09-02 10:55:36 -0400

    base routes

---
+++ server/routes/auth.routes.js
@@ -0,0 +1,11 @@
+import express from 'express'
+import authCtrl from '../controllers/auth.controller';
+
+const router = express.Router()
+
+router.route('/api/auth/signin')
+    .post(authCtrl.signin)
+
+router.route('/api/auth/signout')
+    .get(authCtrl.signout)
+export default router
